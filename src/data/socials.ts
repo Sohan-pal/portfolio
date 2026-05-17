3fdf9aa894a600ae3260ded326d165d329e5ffbf
Sohan Pal <sohanxpal18@gmail.com>
2026-05-17 09:30:40 +0530

    added socials

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,9 +2,9 @@ import { Github, Linkedin, Instagram, Dribbble, Mail } from "lucide-react";
 import { profile } from "./profile";
 
 export const socials = [
-  { label: "Email", href: `mailto:${profile.email}`, icon: Mail },
-  { label: "Dribbble", href: "https://dribbble.com", icon: Dribbble },
-  { label: "Instagram", href: "https://instagram.com", icon: Instagram },
-  { label: "LinkedIn", href: "www.linkedin.com/in/sohan-pal-b79b33309", icon: Linkedin },
+
+  { label: "Dribbble", href: "https://dribbble.com/sohanxpal18", icon: Dribbble },
+  { label: "Instagram", href: "https://www.instagram.com/_sohan.pal/", icon: Instagram },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/sohan-pal-b79b33309/", icon: Linkedin },
   { label: "GitHub", href: "https://github.com/Sohan-pal", icon: Github },
 ];
